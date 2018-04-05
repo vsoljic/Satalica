481820616bfee6eb7edf6885ef8b40fac2243480
Vedrana Šoljić <vedrana.soljic1@gmail.com>
2018-04-05 16:40:16 +0200

    New design, added game page

--- app/src/main/java/tvz/android/satalica/UserActivity.java
+++ app/src/main/java/tvz/android/satalica/UserActivity.java
@@ -3,6 +3,8 @@ package tvz.android.satalica;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,8 +12,13 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
 
-        // Intent intent = getIntent();
-
+    public void startGame(View view) {
+        Intent intent = new Intent(this, Game.class);
+        EditText inputField = findViewById(R.id.inputUserName);
+        String username = inputField.getText().toString();
+        intent.putExtra("username", username);
+        startActivity(intent);
     }
 }
