2e9a61ca977ba005e87ae715d62ddaaa769b51dd
MihaMasic <mmasic@tvz.hr>
2018-04-11 22:51:07 +0200

    Localization

--- app/src/main/java/tvz/android/satalica/activities/MainActivity.java
+++ app/src/main/java/tvz/android/satalica/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package tvz.android.satalica.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import java.util.Locale;
 
 import tvz.android.satalica.R;
 
@@ -27,7 +30,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTutorial(View view) {
-        Intent intent = new Intent (this, TutorialActivity.class);
+        Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+    }
+
+    public void setLocale(View view) {
+        String language;
+        switch (view.getId()) {
+            case (R.id.hr):
+                language = "hr";
+                break;
+            default:
+                language = "en";
+                break;
+        }
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+
+        getResources().updateConfiguration(configuration, getResources().getDisplayMetrics());
+
+        Intent intent = getIntent();
+        finish();
         startActivity(intent);
     }
 }
